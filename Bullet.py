e843a98f52fc8be06593c1a5748a6b3b900122cf
Syed Muhammad Shuja Haider <xplanthris@gmail.com>
2024-04-07 22:07:29 +0300

    Make an enum for the bullet types

---
+++ Bullet.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Bullet(Enum):
+    """
+    An enumeration for representing the types of bullets in the SimpleBuckshotRouletteCounter.
+
+    This enumeration distinguishes between live and blank bullets, as well as whether a bullet
+    has been marked by using the burner phone feature, which requires special dealing with in the autofill function.
+    """
+
+    # Standard bullets that have not been marked through the burner phone.
+    UNKNOWN = 0
+    BLANK = 1
+    LIVE = 2
+
+    # Bullets that have been marked by the burner phone.
+    # These special types allow the autofill function to differentiate between regular and marked bullets,
+    # ensuring accurate autofill behavior.
+    BLANK_MARKED = 3
+    LIVE_MARKED = 4
